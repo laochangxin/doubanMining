8c8d673a8b096b36538930ae297d860fe17e4620
Johnson Lo <laochangxin@qq.com>
2015-09-24 20:12:59 +0800

    Add ProxyCrawler, Fetcher 为爬虫增加代理功能

---
+++ doubanMining/doubanMining/spider/Fetcher.py
@@ -0,0 +1,43 @@
+﻿import urllib2
+import os
+import logging
+import gzip
+import StringIO
+import logging
+import random
+
+import configure
+import ProxyCrawler
+
+class Fetcher(object):
+    """Get url from the web"""
+    def __init__(self, timeout=10, compress=True):
+        self.logger = logging.getLogger(__name__)
+        self.conf = configure.Configure()
+        self.isCompress = compress
+        self.load_proxy_pool()
+
+    PROXY_FILE_PATH = './data/proxy.txt'
+    def load_proxy_pool(self):
+        self.proxy_pool = []
+        proxy_file = Fetcher.PROXY_FILE_PATH
+        if not os.path.exists(proxy_file):
+            proxy_crawler = ProxyCrawler.ProxyCrawler()
+            ret = proxy_crawler.run()
+            if ret != 0:
+                self.logger.warning('Proxy crawler run failed!')
+        f = open(proxy_file, 'r')
+        for line in open(f):
+            linelist = line.strip().split('=')
+            protocol, proxy = linelist
+            self.proxy_pool.append((protocol, proxy))
+        self.logger.info('Proxy pool count: %d' % len(self.proxy_pool))
+        
+    def url_fetch(self, url):
+        user_agent = random.choice(self.conf.user_agents)
+        headers = {
+            'Uesr-Agent': user_agent,
+            "Accept-Encoding": "gzip,deflate",
+            "Accept-Charset" : "UTF-8,*"
+            }
+        pass
---
+++ doubanMining/doubanMining/spider/ProxyCrawler.py
@@ -0,0 +1,43 @@
+﻿from bs4 import BeautifulSoup
+import urllib2
+import httplib
+
+class ProxyCrawler(object):
+    """@Brief: Crawl proxy host&port from a proxy providing web"""
+    def run(self):
+        of = open('proxy.txt' , 'w')
+        for page in range(1, 10):
+            html_doc = urllib2.urlopen('http://www.xici.net.co/nn/' + str(page) ).read()
+            soup = BeautifulSoup(html_doc, 'html.parser')
+            trs = soup.find('table', id='ip_list').find_all('tr')
+            for tr in trs[1:]:
+                tds = tr.find_all('td')
+                ip = tds[2].text.strip()
+                port = tds[3].text.strip()
+                protocol = tds[6].text.strip()
+                if protocol == 'HTTP' or protocol == 'HTTPS':
+                    of.write('%s=%s:%s\n' % (protocol, ip, port) )
+                    print '%s=%s:%s' % (protocol, ip, port)
+        of.close()
+        return 0
+
+if __name__ == '__main__':
+#    proxy_crawler = ProxyHandler()
+#    proxy_crawler.run()
+    proxy_list = []
+    for line in open('proxy.txt', 'r'):
+        linelist = line.strip().split('=')
+        proxy_list.append((linelist[0], linelist[1]))
+
+    url = 'http://movie.douban.com/subject/1292063/?from=subject-page'
+    for protocol, proxy in proxy_list:
+        try:
+            conn = httplib.HTTPConnection(proxy, timeout=3.0)
+            conn.request('GET', url)
+            res = conn.getresponse()
+            content = res.read()
+            writer = open('./data/proxy/%s.html' % proxy.split(':')[0], 'w')
+            writer.write(content)
+            writer.close()
+        except Exception as err:
+            print err
